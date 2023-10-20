e1ba7f916dc8b52f45d847c45eb658b31ab45a8a
metinagalina <148588121+metinagalina@users.noreply.github.com>
2023-10-20 16:36:26 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My new healthy breakfast')
 
 steamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
